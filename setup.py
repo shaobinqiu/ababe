5fde9304435b897341bfea07ad79f99dfa0a8e01
Jason Yu <unkcpz.yu@yahoo.com>
2017-05-31 23:22:35 +0800

    package setup.py reset and add more info

--- setup.py
+++ setup.py
@@ -2,11 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='ababe',
-    version='0.1',
+    version='0.1.0',
     description='A grid structure machine learning tool used in material science.',
     author='Jason Yu',
     author_email='unkcpz.yu@yahoo.com',
     license='MIT',
     packages=find_packages(),
-    package_data={"ababe.stru": ["*.json"]}
+    package_data={"ababe.stru": ["*.json"]},
+    install_requires=["nose>=1.3", "numpy==1.12", 
+                     "spglib==1.9.9.18", "PyYAML==3.11",
+                     "scipy==0.18.1"],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Intended Audience :: Science/Research",
+        "License :: MIT License",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics"]
     )
