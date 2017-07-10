8f04af18d1aa687e0fa1f4600a3672e5bc399d61
Jason Yu <unkcpz.yu@yahoo.com>
2017-07-10 10:14:26 +0800

    test_dsutils test to nose2

--- ababe/mlearn/tests/test_dsutils.py
+++ ababe/mlearn/tests/test_dsutils.py
@@ -2,15 +2,14 @@
 # Distributed under the terms of the MIT License.
 
 import random
+import unittest
 
 from ababe.mlearn.dsutils import RandomPopSet
-import nose
 
-from nose.tools import *
 # from unittest.mock import patch
 
 # @patch('random.randint', return_value=3)
-class test_random_pop_set:
+class RandomPopSet(unittest.TestCase):
 
     def setUp(self):
         self.rp = RandomPopSet()
@@ -20,24 +19,25 @@ class test_random_pop_set:
         rp = RandomPopSet()
         rp.add(5)
         rp.add(5)
-        eq_(rp, RandomPopSet([5]))
+        self.assertEqual(rp, RandomPopSet([5]))
         rp.add(34)
-        eq_(rp, RandomPopSet([5, 34]))
+        self.assertEqual(rp, RandomPopSet([5, 34]))
 
     def test_len(self):
-        eq_(len(self.rp_nn), 4)
-        eq_(len(self.rp), 0)
+        self.assertEqual(len(self.rp_nn), 4)
+        self.assertEqual(len(self.rp), 0)
 
     def test_discard(self):
         rp_nn = RandomPopSet([2,4,5,5,1])
         rp_nn.discard(2)
-        eq_(rp_nn, RandomPopSet([5,5,5,1,4]))
+        self.assertEqual(rp_nn, RandomPopSet([5,5,5,1,4]))
 
     def test_random_pop(self):
         rp_nn = RandomPopSet([2,4,5,5,1])
         random.seed(0)
-        eq_(rp_nn.random_pop(), 1)
-        eq_(rp_nn, RandomPopSet([2,4,5]))
+        self.assertEqual(rp_nn.random_pop(), 1)
+        self.assertEqual(rp_nn, RandomPopSet([2,4,5]))
 
 if __name__ == "__main__":
-    nose.main()
+    import nose2
+    nose2.main()
