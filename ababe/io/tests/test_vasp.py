f4602a68ba827642d20c49eb0e2088738644fb5d
Jason.Yu <unkcpz.yu@yahoo.com>
2017-09-23 09:11:12 +0800

    vasp input continuing

--- ababe/io/tests/test_vasp.py
+++ ababe/io/tests/test_vasp.py
@@ -1,2 +1,17 @@
 # coding: utf-8
 # Distributed under the terms of the MIT License.
+import unittest
+
+class TestVaspInput(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = os.path.join(testdata_dir, )
+
+class TestVaspOutput(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+if __name__ == "__main__":
+    import nose2
+    nose2.main()
--- ababe/io/tests/test_yaml.py
+++ ababe/io/tests/test_yaml.py
@@ -17,15 +17,42 @@ class TestYamlInput(unittest.TestCase):
     def test_init(self):
         yaml_in = YamlInput(self.filename)
 
+    def test_from_string(self):
+        yaml_in = YamlInput(self.filename)
+        content = """comment: zb_ZnS
+lattice:
+  - [0.0, 0.5, 0.5]
+  - [0.5,  0.0,  0.5]
+  - [ 0.5, 0.5,  0.0]
+positions:
+  - [0., 0., 0.]
+  - [0.25, 0.25, 0.25]
+numbers: [30, 16]
+zoom: 4
+"""
+        latt, pos, numbers = yaml_in.from_string(content)
+        expect_latt = np.array([[0., 2., 2.],
+                                [2., 0., 2.],
+                                [2., 2., 0.]])
+        expect_positions = np.array([[0., 0., 0.],
+                                     [0.25, 0.25, 0.25]])
+        expect_numbers = np.array([30, 16])
+
+        self.assertTrue(np.allclose(latt, expect_latt))
+        self.assertTrue(np.allclose(pos, expect_positions))
+        self.assertTrue(np.allclose(numbers, expect_numbers))
+
+
+
     def test_get_gcell(self):
         yaml_in = YamlInput(self.filename)
         gcell = yaml_in.get_cell()
 
-        expect_latt = np.array([[0., 0.5, 0.5],
-                                [0.5, 0., 0.5],
-                                [0.5, 0.5, 0.]])
+        expect_latt = np.array([[0., 2., 2.],
+                                [2., 0., 2.],
+                                [2., 2., 0.]])
         expect_positions = np.array([[0., 0., 0.],
-                                     [0.25, 0.25, 0.25])
+                                     [0.25, 0.25, 0.25]])
         expect_numbers = np.array([30, 16])
         self.assertTrue(np.allclose(gcell.lattice, expect_latt))
         self.assertTrue(np.allclose(gcell.positions, expect_positions))
@@ -39,21 +66,21 @@ class TestYamlOutput(unittest.TestCase):
                          [0.5, 0., 0.5],
                          [0.5, 0.5, 0.]])
         positions = np.array([[0., 0., 0.],
-                              [0.25, 0.25, 0.25])
+                              [0.25, 0.25, 0.25]])
         numbers = np.array([30, 16])
         cell = GeneralCell(latt, positions, numbers)
         self.yaml_out = YamlOutput(cell, comment=None, zoom=3)
 
     def test_get_string(self):
-        expected_str = '''comment: S6Zn2
+        expected_str = '''comment: S1Zn1
 lattice:
-- [0.000000, 0.500000, 0.500000]
-- [0.500000, 0.000000, 0.500000]
-- [0.500000, 0.500000, 0.000000]
+- [0.0, 0.5, 0.5]
+- [0.5, 0.0, 0.5]
+- [0.5, 0.5, 0.0]
+numbers: [30, 16]
 positions:
-- [0.250000, 0.250000, 0.250000]
-- [0.000000, 0.000000, 0.000000]
-numbers: [16, 30]
+- [0.0, 0.0, 0.0]
+- [0.25, 0.25, 0.25]
 zoom: 3
 '''
         self.assertEqual(str(self.yaml_out), expected_str)
@@ -66,7 +93,7 @@ zoom: 3
         origin input parameter. Using almostEqual
         """
         tmp_file = os.path.join(testdata_dir, "testing.yaml")
-        self.yaml_out.write(tmp_file)
+        self.yaml_out.write_file(tmp_file)
 
         with open(tmp_file, 'r') as testing_file:
             data = testing_file.read()
@@ -75,6 +102,6 @@ zoom: 3
         os.remove(tmp_file)
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     import nose2
     nose2.main()
