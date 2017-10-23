5c08f7073852f56a48bccef94d121e45853aaf8b
Jason.Yu <unkcpz.yu@yahoo.com>
2017-10-23 15:18:26 +0800

    maker ternary use new io system

--- ababe/cmdline/apps/occupymakert.py
+++ ababe/cmdline/apps/occupymakert.py
@@ -4,7 +4,7 @@ from .model import AppModel
 from ababe.stru.element import Specie
 from ababe.stru.scaffold import GeneralCell
 from ababe.stru.sogen import OccupyGenerator
-from ababe.stru.io import VaspPOSCAR, YamlOutput
+from ababe.io.io import GeneralIO
 from ababe.stru.restriction import MinDistanceRestriction
 
 import numpy as np
@@ -12,32 +12,18 @@ import os
 
 class App(AppModel):
 
-    def __init__(self, settings, comment, element, speckle, zoom, trs,
+    def __init__(self, infile, comment, element, speckle, trs,
                  refined, outmode):
         # read comment & zoom from setting file first
         # if not exist, read from cmd args, then default
-        if zoom is None:
-            try:
-                zoom = settings['zoom']
-            except:
-                zoom = 1
-        self.zoom = zoom
+        self.cell = GeneralIO.from_file(infile)
 
-        if comment is None:
-            try:
-                comment = settings['comment']
-            except:
-                comment = 'default'
-        self.comment = comment
-
-        lat = np.array(settings['lattice'])
-        pos = np.array(settings['positions'])
-        num = np.array(settings['numbers'])
-        self.cell = GeneralCell(lat*self.zoom, pos, num)
+        self.comment = comment or self.cell.comment
 
         self.element = element
 
         # Get number and index of target element
+        num = self.cell.numbers
         if element is None:
             tgt_ele = int(num[1])
         else:
@@ -62,19 +48,18 @@ class App(AppModel):
         # Create directory contain POSCARs
         import random
         import string
-        import tempfile
 
         rd_suffix = ''.join(random.choices(string.ascii_uppercase
                                            + string.digits, k=5))
         working_path = os.getcwd()
-        poscars_dir = os.path.join(working_path,
+        out_dir = os.path.join(working_path,
                                    'STRUCTURES_{0:}_{1:}'.format(self.comment,
                                                               rd_suffix))
-        if not os.path.exists(poscars_dir):
-            os.makedirs(poscars_dir)
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
         else:
-            shutil.rmtree(poscars_dir)
-            os.makedirs(poscars_dir)
+            shutil.rmtree(out_dir)
+            os.makedirs(out_dir)
 
         ogg = OccupyGenerator(self.cell)
 
@@ -82,15 +67,6 @@ class App(AppModel):
             tr = (Specie(self.tr[0]), self.tr[1])
             applied_restriction = MinDistanceRestriction(tr)
 
-        # For diff outmode
-        if self.outmode == 'vasp':
-            Output = VaspPOSCAR
-            prefix = 'POSCAR_A{:}B{:}C{:}_'
-            suffix = '.vasp'
-        else:
-            Output = YamlOutput
-            prefix = 'STRUCTURE_A{:}B{:}C{:}_'
-            suffix = '.yaml'
 
         for n1 in range(1, self.n - 1):
             for n2 in range(1, self.n - n1):
@@ -106,9 +82,10 @@ class App(AppModel):
                     if condition:
                         if self.refined:
                             c = c.get_refined_pcell()
-                        poscar = Output(c, 1)
-                        tf = tempfile.NamedTemporaryFile(mode='w+b', dir=poscars_dir,
-                                                         prefix=prefix.format(self.n - n1 -n2,
-                                                                              n1, n2),
-                                                         suffix=suffix, delete=False)
-                        poscar.write(tf.name)
+                        out = GeneralIO(c)
+
+                        f_suffix = ''.join(random.choices(string.ascii_uppercase
+                                                          + string.digits, k=4))
+                        ofname = "STRUCTURE_{:}_{:}.{:}".format(c.comment, f_suffix, self.outmode)
+                        lastpath = os.path.join(out_dir, ofname)
+                        out.write_file(lastpath)
--- ababe/cmdline/runabalib.py
+++ ababe/cmdline/runabalib.py
@@ -93,17 +93,15 @@ def ocutenter(input, comment, element, speckle, nspeckle, zoom, trs, refined, ou
 
 @exec_from_cmdline.command()
 @click.argument('input', type=click.Path(exists=True))
-@click.option('--comment', default=None)
-@click.option('--element', default=None)
-@click.option('--speckle', nargs=2)
-@click.option('--zoom', type=float, default=None)
+@click.option('--comment', '-c', default=None)
+@click.option('--element', '-e', default=None)
+@click.option('--speckle', '-s', nargs=2)
 @click.option('--dist-restrict', '-r', 'trs', nargs=2, type=click.Tuple([str, float]), multiple=True)
 @click.option('--refined/--no-refined', default=True)
-@click.option('--outmode', type=click.Choice(['vasp', 'yaml']), default='vasp')
-def ocumakert(input, comment, element, speckle, zoom, trs, refined, outmode):
+@click.option('--outmode', '-o', type=click.Choice(['vasp', 'yaml']), default='vasp')
+def ocumakert(input, comment, element, speckle, trs, refined, outmode):
     infile=click.format_filename(input)
-    y = yaml.load(open(infile, "r"))
 
-    appoccupymakert = occupymakert.App(y, comment, element, speckle,
-                                       zoom, trs, refined, outmode)
+    appoccupymakert = occupymakert.App(infile, comment, element, speckle,
+                                       trs, refined, outmode)
     appoccupymakert.run()
